5f3246b277c3e37cd2617fdb3f51da73eba926e7
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2010-03-13 06:46:50 +0100

    Outputting the key in a textarea, and using $nid instead of $node->nid.

--- sshaccess_public_key.tpl.php
+++ sshaccess_public_key.tpl.php
@@ -1,4 +1,4 @@
 <div class="sshaccess-remote-account-public-key">
   <p class="message"><?php print t('This public key must be listed in the authorized_keys file for the remote account.'); ?></p>
-  <p class="public-key"><?php print check_plain($pubkey['pubkey']); ?></p>
+  <textarea class="public-key" cols="50" rows="10"><?php print check_plain($pubkey['pubkey']); ?></textarea>
 </div>
